cb41ae501fd3a9ad0c9f6acff68737bb364c0e35
Christopher <conrad317@gmail.com>
2018-06-25 23:09:40 -0700

    complete

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Challenge
+Implement a Queue using two Stacks.
 
-Write a method for the Linked List class which takes a number, k, as a parameter. Return the node that is k from the end of the linked list.
 
 ## Solution
-![]https://photos.app.goo.gl/gZ3ivoRfKcP352Vq9
+
---
+++ queue_with_stacks.js
@@ -0,0 +1,23 @@
+'use strict'
+
+class Queue {
+  constructor () {
+    this.arr = ['2','3','4','5'];
+    this.arr2 = ['1'];
+    this arr3 = ['1','2']
+  }
+
+  enqueue(value) {
+    for (let i = 0; i< this.arr.length; i +=1) {
+      this.arr.push(this.arr.pop());
+    }
+    this.arr.push(value);
+    for (let i = 0; i < this.arr2.length; i += 1) {
+
+    }
+  }
+
+  dequeue() {
+    return this.arr3.pop();
+  }
+};
